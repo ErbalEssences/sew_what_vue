1fc50f3f2c06cad8174ec3f297ab2b96022b1a2e
ErbalEssences <LieseErb.com>
2019-06-26 09:47:59 -0500

    working on user closets

--- src/views/UsersShow.vue
+++ src/views/UsersShow.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="recipes-show">
+  <div class="users-show">
     <div class="container">
       <div class="row">
 
@@ -16,14 +16,16 @@
             <div class="w-100"></div>
         </div> -->
 
-
-
         <div class="col-sm-6">
-
           <h1>{{ user.username }}</h1>
           <h3>Skill Level: {{ user.skill }}</h3>
-          <router-link class="btn btn-warning m-1" v-bind:to=" '/users/' + user.id + '/edit' ">Edit</router-link>
-          <button class="btn btn-warning m-1" v-on:click="destroyuser()">Delete</button>
+          <h4>Total Closets: {{ user.number_of_closets }}</h4>
+          <h4>Total Patterns: {{ user.number_of_patterns }}</h4>
+          <router-link class="btn btn-warning m-1" v-bind:to=" '/users/' + user.id + '/edit' ">Edit or Delete</router-link>
+          
+          <router-link class="btn btn-warning m-1" v-bind:to=" '/closet/' + user.id + '/edit' ">New Closet</router-link>
+
+          <!-- <button class="btn btn-warning m-1" v-on:click="destroyuser()">Delete</button> -->
         </div>
         <div class="col-sm-6">
           <img v-bind:src="user.avatar_url" class="img-fluid show-user-img">
@@ -31,61 +33,24 @@
 
 
 
-        <div v-for="closets in user">
-          <div v-for="closet in closets">
-            <div v-for="patterns in closet">
-              <div v-for="pattern in patterns">
-                <div v-for="images in pattern">
-                  <div v-for="main_images in images">
-
-
-                    <div class="card m-1" style="width: 18rem;">
-                      
-                      <img class="card-img-top" v-bind:src="main_images.first" alt="Closet Image">
-
-                      <div class="card-body">
-                        <h1>{{ main_images }} ...</h1>
-                        <a href="#" class="btn btn-warning">{{closet.name}} Closet</a>
-                      </div>
-
-                    </div>
-
-                  </div>
-                </div>
-              </div>
+        <div v-for="closet in user.closets">
+          <div class="card m-4" style="width: 18rem;">
+            <img class="card-img-top" v-bind:src="closet.patterns[0].images.main_images[0].url" alt="Closet Image">
+            <div class="card-body">
+              <a href="#" class="btn btn-warning">{{closet.name}} Closet</a>
             </div>
           </div>
         </div>
 
-
-
-<!--         <div v-for="closets in user">
-          <div v-for="closet in closets">
-            <div class="card m-1" style="width: 18rem;">
-
-              <img class="card-img-top" v-bind:src="user.avatar_url" alt="Closet Image">
-
-              <div class="card-body">
-                <a href="#" class="btn btn-warning">{{closet.name}} Closet</a>
-              </div>
-            
-            </div>
-          </div>
-        </div> -->
-
-
       </div>
     </div>
-
-    
-
-
   </div>
 </template>
 
 <style>
 img.show-user-img {
-  width: 250px;
+  max-width: 450px;
+  max-height: 300px;
 }
 
 .btn-my-color {
@@ -100,77 +65,81 @@ export default {
   data: function() {
     return {
       user: {
-              "username": "",
-              "email": "",
-              "skill": "",
-              "avatar_url": "",
-              "closets": [
+               username: "",
+               email: "",
+               skill: "",
+               avatar_url: "",
+               closets: [
                           {
-                            "name": "Dresses",
-                            "user_id": 1,
-                            "patterns": [
+                             name: "",
+                             user_id: 1,
+                             patterns: [
                                           {
-                                            "name": "",
-                                            "url": "",
-                                            "price": "",
-                                            "out_of_print": false,
-                                            "display_name": "",
-                                            "images": {
-                                                        "line_art": {
-                                                                      "pattern_id": 1,
-                                                                      "user_id": null,
-                                                                      "url": "",
-                                                                      "line_art": true
+                                             name: "",
+                                             url: "",
+                                             price: "",
+                                             out_of_print: false,
+                                             display_name: "",
+                                             images: {
+                                                         line_art: {
+                                                                       pattern_id: 1,
+                                                                       user_id: null,
+                                                                       url: "",
+                                                                       line_art: true
                                                                     },
-                                                        "main_images": [
+                                                         main_images: [
                                                                           {
-                                                                            "pattern_id": 1,
-                                                                            "user_id": null,
-                                                                            "url": "",
-                                                                            "line_art": false
+                                                                             pattern_id: 1,
+                                                                             user_id: null,
+                                                                             url: "",
+                                                                             line_art: false
                                                                           }
                                                                         ],
-                                                        "user_images": []
+                                                         user_images: [],
                                                       },
-                                          "tags": [
-                                                    {
-                                                      "id": 1,
-                                                      "name": ""
-                                                    }
-                                          ]
+                                             tags: [
+                                                      {
+                                                         id: 1,
+                                                         name: ""
+                                                      }
+                                                    ]
                                           }
-                                          ],
-              "notes": {
-                          "private": [
-                                      {
-                                          "pattern_id": 1,
-                                          "user_id": 1,
-                                          "body": "",
-                                          "public": false
-                                      }
-                                          ],
-                          "public": [
-                                      {
-                                        "pattern_id": 1,
-                                        "user_id": 1,
-                                        "body": "",
-                                        "public": true
-                                      }
                                         ]
                           }
-            },
+                         ],
+               notes: {
+                           private: [
+                                        {
+                                           pattern_id: 1,
+                                           user_id: 1,
+                                           body: "",
+                                           public: false
+                                        },
+                                     ],
+                           public: [
+                                      {
+                                         pattern_id: 1,
+                                         user_id: 1,
+                                         body: "",
+                                         public: true
+                                      },
+                                    ]
+                          },
 
+            },
+          };
+    },
   created: function() {
     axios.get("/api/users/" + this.$route.params.id ).then(response => {
       this.user = response.data;
     });
   },
-  methods: {
-    destroyUser: function() {
-      axios.delete("/api/recipes/" + this.recipe.id).then(response => {
-        this.$router.push("/");
-      });
-    }
-  }
+  // methods: {
+  //   destroyUser: function() {
+  //     axios.delete("/api/users/" + this.user.id).then(response => {
+  //       this.$router.push("/");
+  //     });
+  //   }
+  // }
 };
 </script>
